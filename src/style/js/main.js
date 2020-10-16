472d4b9a9fdf418e1205e468eedbf17ceb33f596
Eugene Rudenko <egnrdnk@gmail.com>
2020-10-16 17:56:11 +0300

    Add help for address input

--- src/style/js/main.js
+++ src/style/js/main.js
@@ -586,12 +586,82 @@ $(function() {
 	//
 
 	//
-	// Table checkbox
+	// Shipment datalist
 	//
 
-	//
-	// Table checkbox end
-	//
+	class DataList {
+		constructor(containerId, inputId, listId, options) {
+			this.containerId = containerId;
+			this.inputId = inputId;
+			this.listId = listId;
+			this.options = options;
+		}
+	
+		create(filter = "") {
+			const list = document.getElementById(this.listId);
+			const filterOptions = this.options.filter(
+				d => filter === "" || d.text.includes(filter)
+			);
+	
+			if (filterOptions.length === 0) {
+				list.classList.remove("active");
+			} else {
+				list.classList.add("active");
+			}
+	
+			list.innerHTML = filterOptions
+				.map(o => `<li id=${o.value}>${o.text}</li>`)
+				.join("");
+		}
+	
+		addListeners(datalist) {
+			const container = document.getElementById(this.containerId);
+			const input = document.getElementById(this.inputId);
+			const list = document.getElementById(this.listId);
+			container.addEventListener("click", e => {
+				if (e.target.id === this.inputId) {
+					container.classList.toggle("active");
+				} else if (e.target.id === "datalist-icon") {
+					container.classList.toggle("active");
+					input.focus();
+				}
+			});
+	
+			input.addEventListener("input", function(e) {
+				if (!container.classList.contains("active")) {
+					container.classList.add("active");
+				}
+	
+				datalist.create(input.value);
+			});
+	
+			list.addEventListener("click", function(e) {
+				if (e.target.nodeName.toLocaleLowerCase() === "li") {
+					input.value = e.target.innerText;
+					container.classList.remove("active");
+				}
+			});
+		}
+	}
+	
+	const data = [
+		{ value: "1", text: "Россия г. Москва ул. Ангарская вл. 8, стр. 15 д." },
+		{ value: "2", text: "Россия г. Москва ул. Ангарская вл. 9, стр. 15 д." },
+		{ value: "3", text: "Россия г. Москва ул. Ангарская вл. 10, стр. 15 д." },
+		{ value: "4", text: "Россия г. Москва ул. Ангарская вл. 11, стр. 15 д." },
+	];
+	
+	const datalist = new DataList(
+		"shipment-datalist",
+		"shipment-datalist-input",
+		"shipment-datalist-list",
+		data
+	);
+	datalist.create();
+	datalist.addListeners(datalist);
 
+	//
+	// Shipment datalist end
+	//
 
 });
